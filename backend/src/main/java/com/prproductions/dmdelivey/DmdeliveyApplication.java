fb8a9154c820d16d857506fe39ceb36198d3325d
Paulo Goncalves <paullogoncalves@gmail.com>
2021-01-09 20:09:36 -0300

    Initial commit

---
+++ backend/src/main/java/com/prproductions/dmdelivey/DmdeliveyApplication.java
@@ -0,0 +1,13 @@
+package com.prproductions.dmdelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmdeliveyApplication.class, args);
+	}
+
+}
